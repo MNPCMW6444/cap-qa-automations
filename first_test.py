7c5a1bbd1da4834037d421cdf1296fda0081cfb2
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-24 23:14:29 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -3,12 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome() # Replace with your desired browser's webdriver, e.g., webdriver.Firefox()
+        self.driver = webdriver.Chrome()
 
     def test_user_can_use_app(self):
         driver = self.driver
@@ -17,19 +18,25 @@ class TestApp(unittest.TestCase):
         wait = WebDriverWait(driver, 10)  # 10 seconds timeout
         email = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-testid="email"]')))
         password = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-testid="password"]')))
-        login_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-testid="login-button"]')))
+        login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="login-button"]')))
+        actions = ActionChains(driver)
+        actions.move_to_element(login_button).click().perform()
 
-        email.send_keys("flighter98311+1@gmail.com")  # Replace with test user's email
-        password.send_keys("daniel1998$&@")  # Replace with test user's password
-        login_button.click()
-
+        email.send_keys(Keys.CONTROL + "a")
+        
+        email.send_keys("flighter98311+1@gmail.com")  
+        password.send_keys("daniel1998$&@")  
+        login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="login-button"]')))
+        actions = ActionChains(driver)
+        actions.move_to_element(login_button).click().perform()
         time.sleep(15)  # Wait for the page to load
 
         # Check the result (replace with your app's specific result checks)
         result_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//p[text()='home']")))
         self.assertEqual(result_element.text, "home")
 
-    def tearDown(self):
+
+def tearDown(self):
         self.driver.close()
 
 if __name__ == "__main__":
